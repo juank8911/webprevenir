b46eca1ec55837158598ad0bd99bb4c21fb443d6
Stefanía Enríquez <ruthstefania28@gmail.com>
2019-04-06 12:28:37 -0500

    Primer Commit

---
+++ modalInicio.php
@@ -0,0 +1,17 @@
+ <div class="modal fade" id="modalInicio" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+       <div class="modal-content">
+         <div class="modal-header">
+            <button type="button" class="Cerrar" data-dismiss="modal">&times;</button>
+<!--            <h4 class="modal-title">Emergencias 24H</h4>-->
+         </div>
+         <div class="modal-body" width="50%" height="10%" >
+             <img src="images/prevenir/GifLíneasFunenrarias.gif" width="70%" height="100%" />
+         </div>
+         <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
---
+++ nav.php
@@ -0,0 +1,46 @@
+        <nav class="navbar navbar-default navbar-static-top pull-left">            
+
+            <div class="">  
+              <ul class="nav navbar-nav sf-menu" data-type="navbar">
+                <li class="active">
+                  <a href="./">Inicio</a>
+                </li>
+                <li>
+                    <a href="views/quienes/somos.php">Quienes Somos</a>
+                </li>
+                <li class="dropdown">
+                  <a href="#">Planes<span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span></a>
+                  <ul class="dropdown-menu">
+                    <li>
+                        <a href="views/planes/individual.php">Individual</a>
+                    </li>
+                    <li>
+                        <a href="views/planes/descuento.php">Descuento Medico</a>                      
+                    </li>
+                    <li>
+                        <a href="views/planes/patriarca.php">Patriarca</a>                      
+                    </li>
+                    <li>
+                        <a href="views/planes/mascota.php">Mascotas</a>                      
+                    </li>
+<!--                    <li>
+                      <a href="#">Patriarca</a>
+                        <ul class="dropdown-menu">
+                          <li>
+                            <a href="#">Mascotas</a>
+                          </li>                    
+                        </ul>
+                    </li>-->
+                  </ul>
+                </li>                
+                <li>
+                    <a href="views/pqrs/index.php">PQRS</a>
+                </li>
+                
+                <li>
+                    <a href="views/terminos/condiciones.php">Terminos y Condiciones</a>
+                </li>
+              </ul>                           
+            </div>
+        </nav>   
+        
